e0af8ecb5a546b1c12f736931237f122677df18d
JJ <956429781@qq.com>
2018-08-29 08:58:11 +0800

    jjsunyet0829

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FooterGuide v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+  mounted () {
+    // 分发给action发ajax获取address数据
+    this.$store.dispatch('getAddress')
+  },
+
+  name: 'App',
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+#app
+    width 100%
+    height 100%
+</style>
